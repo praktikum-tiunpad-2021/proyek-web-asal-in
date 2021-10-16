c20be43d20cd39c82d443a6b82d58a2f52ea7d2e
AncuL001 <ancul001@gmail.com>
2021-10-16 23:06:20 +0700

    tidy up navbar

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,12 +1,11 @@
-<header style="background-image: url(img/perpustakaan.jpg);">
-  <br>
-  <br>
-  <center><img class="GFG" src="img/logosppo.png"></center>
-  <br>
-  <br>
+<header>
+  <div class="logo-row">
+    <br><br>
+    <img class="logo" src="img/logosppo.png">
+    <br><br>
   </div>
-
-  <nav class="topnav">
+  
+  <nav>
     <a href="/">Home</a>
     <a href="#berita">Berita</a>
     <a href="/hubungi">Hubungi</a>
