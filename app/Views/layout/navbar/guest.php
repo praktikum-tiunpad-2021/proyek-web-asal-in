1119d477bd413293a1b8194afb8faac7be7e451e
AnggieForestry <anggieforestry@gmail.com>
2021-10-26 20:40:07 +0700

    to base url

--- app/Views/layout/navbar/guest.php
+++ app/Views/layout/navbar/guest.php
@@ -1,6 +1,6 @@
-<a class="nav-item" href="/katalog">Katalog Buku</a>
-<a class="nav-item" href="/hubungi">Hubungi</a>
+<a class="nav-item" href="<?= base_url('katalog') ?>">Katalog Buku</a>
+<a class="nav-item" href="<?= base_url('hubungi')?>">Hubungi</a>
 
-<a href="/masuk" class="button-container">
+<a href="<?= base_url('masuk')?>" class="button-container">
   <button class="btn very-bright">Masuk</button>
 </a>
--- app/Views/layout/navbar/user.php
+++ app/Views/layout/navbar/user.php
@@ -1,5 +1,5 @@
-<a class="nav-item" href="/katalog">Katalog Buku</a>
-<a class="nav-item" href="/hubungi">Hubungi</a>
+<a class="nav-item" href="<?= base_url('katalog') ?>">Katalog Buku</a>
+<a class="nav-item" href="<?= base_url('hubungi') ?>">Hubungi</a>
 
 <a href="/profile" class="button-container">
   <button class="btn gold">Profil</button>
