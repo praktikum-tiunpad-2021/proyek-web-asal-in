38c6c722d08bca7957eeb9b7de9adb0c72650583
AncuL001 <ancul001@gmail.com>
2021-10-27 10:11:16 +0700

    remove base_url

--- app/Views/layout/navbar/user.php
+++ app/Views/layout/navbar/user.php
@@ -1,9 +1,9 @@
-<a class="nav-item" href="<?= base_url('katalog') ?>">Katalog Buku</a>
-<a class="nav-item" href="<?= base_url('hubungi') ?>">Hubungi</a>
+<a class="nav-item" href="/katalog">Katalog Buku</a>
+<a class="nav-item" href="/hubungi">Hubungi</a>
 
-<a href="<?= base_url('profile') ?>" class="button-container">
+<a href="/profile" class="button-container">
   <button class="btn gold">Profil</button>
 </a>
-<a href="<?= base_url('logout') ?>" class="button-container">
+<a href="/logout" class="button-container">
   <button class="btn very-bright">Keluar</button>
 </a>
