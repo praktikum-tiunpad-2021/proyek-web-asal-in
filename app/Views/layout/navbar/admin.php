27543f150f9f98be8ed0a56ddcd384758197ce52
AncuL001 <ancul001@gmail.com>
2021-11-02 14:38:23 +0700

    fix redirect links, change button structure

--- app/Views/layout/navbar/admin.php
+++ app/Views/layout/navbar/admin.php
@@ -1,12 +1,12 @@
-<a class="nav-item" href="<?= base_url('katalog') ?>">Katalog Buku</a>
+<a class="nav-item" href="<?= base_url('buku/katalog') ?>">Katalog Buku</a>
 <a class="nav-item" href="<?= base_url('hubungi') ?>">Hubungi</a>
 
-<a href="<?= base_url('admin') ?>" class="button-container">
-  <button class="btn gold">Admin</button>
-</a>
-<a href="<?= base_url('profile') ?>" class="button-container">
-  <button class="btn gold">Profil</button>
-</a>
-<a href="<?= base_url('logout') ?>" class="button-container">
-  <button class="btn very-bright">Keluar</button>
-</a> 
+<div class="button-container">
+  <a href="<?= base_url('admin') ?>" class="btn gold">Admin</a>
+</div>
+<div class="button-container">
+  <a href="<?= base_url('profil') ?>" class="btn gold">Profil</a>
+</div>
+<div class="button-container">
+  <a href="<?= base_url('logout') ?>" class="btn very-bright">Keluar</a>
+</div>
--- app/Views/layout/navbar/guest.php
+++ app/Views/layout/navbar/guest.php
@@ -1,6 +1,6 @@
-<a class="nav-item" href="<?= base_url('katalog') ?>">Katalog Buku</a>
+<a class="nav-item" href="<?= base_url('buku/katalog') ?>">Katalog Buku</a>
 <a class="nav-item" href="<?= base_url('hubungi')?>">Hubungi</a>
 
-<a href="<?= base_url('masuk')?>" class="button-container">
-  <button class="btn very-bright">Masuk</button>
-</a>
+<div class="button-container">
+  <a href="<?= base_url('masuk') ?>" class="btn very-bright">Masuk</a>
+</div>
