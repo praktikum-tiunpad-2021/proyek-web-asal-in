97081afbcf6482d6fe34d0dd2872e03babedd02b
AnggieForestry <anggieforestry@gmail.com>
2021-11-28 22:39:40 +0700

    ubah gender

--- app/Database/Migrations/2021-11-25-115917_UserProfile.php
+++ app/Database/Migrations/2021-11-25-115917_UserProfile.php
@@ -19,7 +19,7 @@ class UserProfile extends Migration
             ],
             'gender' => [
                 'type' => 'ENUM',
-                'constraint' => ['MALE', 'FEMALE', 'OTHER'],
+                'constraint' => ['Laki-laki', 'Perempuan', 'Lainnya'],
                 'null' => true
             ],
             'date_of_birth' => [
