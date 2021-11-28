efce973bf82022f3d8ea7a419f8f4a02c7cfd6be
AnggieForestry <anggieforestry@gmail.com>
2021-11-29 00:02:23 +0700

    idk

--- app/Views/hubungi.php
+++ app/Views/hubungi.php
@@ -5,9 +5,9 @@
     <h1 class="header-padding">Hubungi</h1>
     Berikut ini beberapa kontak yang dapat dihubungi:
     <ul>
-        <li>Anggie Forestry: +62 812-2401-3119 (Bisa WA)</li>
-        <li>Ananda Shaquille Farell: +62 812-8985-6576 (Bisa WA) | anshafarell@gmail.com</li>
-        <li>Fauzan Azmi Dwicahyo: +62 813-1772-1109 (Bisa WA)</li>
+        <li>Anggie Forestry +62 812-2401-3119 </li>
+        <li>Ananda Shaquille Farell +62 812-8985-6576 </li>
+        <li>Fauzan Azmi Dwicahyo +62 813-1772-1109 </li>
     </ul>
 </main>
 <?= $this->endSection('content'); ?>
