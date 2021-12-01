bbeb81e75b1414df380b3ed1b31e8538137b4e4e
AncuL001 <ancul001@gmail.com>
2021-12-01 08:55:28 +0700

    remove unused pagination files

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,8 +24,6 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'user_pagination' => 'App\Views\Pagers\user_pagination',
-        'book_pagination' => 'App\Views\Pagers\book_pagination'
     ];
 
     /**
--- app/Views/pagers/book_pagination.php
+++
@@ -1,9 +0,0 @@
-<nav aria-label="Page navigation">
-  <div class="pagination">
-    <a href="#">Previous</a>
-    <a href="#">1</a>
-    <a href="#">2</a>
-    <a href="#">3</a>
-    <a href="#">Next</a>
-</div>
-</nav>
