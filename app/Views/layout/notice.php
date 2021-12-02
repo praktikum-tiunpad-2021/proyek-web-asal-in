d1ec3b6d528939ac569cd9a0115a95958c5dfcf6
AncuL001 <ancul001@gmail.com>
2021-12-02 20:15:07 +0700

    beautify

--- app/Views/layout/notice.php
+++ app/Views/layout/notice.php
@@ -1,15 +1,15 @@
-<?php if (session()->getFlashdata('errors') != null) : ?>
-  <div class="error-container">
+<?php if (session()->getFlashdata('error') != null) : ?>
+  <div class="error-container container" style="margin-top: 1em;">
     <ul>
-      <li><?= session()->getFlashdata('errors') ?></li>
+      <li><?= session()->getFlashdata('error') ?></li>
     </ul>
   </div>
 <?php endif ?>
 
-<?php if (session()->getFlashdata('successes') != null) : ?>
-  <div class="success-container">
+<?php if (session()->getFlashdata('success') != null) : ?>
+  <div class="success-container container" style="margin-top: 1em;">
     <ul>
-      <li><?= session()->getFlashdata('successes') ?></li>
+      <li><?= session()->getFlashdata('success') ?></li>
     </ul>
   </div>
 <?php endif ?>
